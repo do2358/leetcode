982b2af332f1ab148863dd5922234c1c284e9ed8
Do <nguyenducdo.hust@gmail.com>
2024-08-01 13:50:30 +0700

    feat: 26,27

--- 150-interview/26/solution.rs
+++ 150-interview/26/solution.rs
@@ -4,7 +4,7 @@ impl Solution {
             return 0;
         }
 
-        let mut j: u32 = 0;
+        let mut j = 0;
         for i in 1..nums.len() {
             if nums[j] != nums[i] {
                 j += 1;
